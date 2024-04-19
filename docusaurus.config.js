96193a82832e7f2b424eb4520d04de8a24b1031c
Publik <sean.peters86@icloud.com>
2024-04-19 14:23:37 -0500

    update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/book-of-shadows.jpg',
       colorMode: {
         defaultMode: 'dark'
       },
